675fcee251a444073c03fbf6d476a8281b087433
liu8086 <104002425liu@gmail.com>
2026-08-16 19:04:06 +0800

    deploy 启动服务-电脑用.bat

---
+++ 启动服务-电脑用.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "%~dp0"
+set "PY=C:\Users\Administrator\.cache\codex-runtimes\codex-primary-runtime\dependencies\python\python.exe"
+if not exist "%PY%" set "PY=python"
+start "fc3d-server" /min "%PY%" -m http.server 8000
+timeout /t 1 /nobreak >nul
+start "" http://localhost:8000/index.html
